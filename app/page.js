82c4d77f2362c224f7bb06da87f058d9c401ea63
Ash-Greninja-Coder <greninjaash449@gmail.com>
2025-12-26 15:19:35 +0530

    Setting Up Routes and Themes With Authentication

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div>Subscribe to road</div>
+    <div>
+      Welcome to sensai
+      <br/>
+        <Button> Hello </Button>
+    </div>
   );
 }
